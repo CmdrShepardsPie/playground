71db1b0c555f84ca3b0029f0aa2afa48aff48b66
Christopher Simpson <CmdrShepardsPie@users.noreply.github.com>
2018-05-04 08:39:50 -0600

    Expand search radius

--- repeater/get-repeaters.js
+++ repeater/get-repeaters.js
@@ -33,15 +33,38 @@
     }
     exports.save = save;
     exports.default = (async () => {
-        for (let i = 0; i <= 100; i++) {
-            await save('Denver, CO', i);
+        for (let i = 0; i <= 200; i++) {
             await save('Colorado Springs, CO', i);
-            await save('Woodland Park, CO', i);
+            await save('Monument, CO', i);
+            await save('Castle Rock, CO', i);
+            await save('Castle Pines, CO', i);
+            await save('Centennial, CO', i);
+            await save('Parker, CO', i);
+            await save('Lakewood, CO', i);
+            await save('Denver, CO', i);
+            await save('Golden, CO', i);
             await save('Idaho Springs, CO', i);
-            await save('Boulder, CO', i);
-            await save('Ouray, CO', i);
-            await save('Buena Vista, CO', i);
+            await save('Evergreen, CO', i);
+            await save('Lone Tree, CO', i);
+            await save('Empire, CO', i);
+            await save('Silverthorne, CO', i);
+            await save('Frisco, CO', i);
+            await save('Vail, CO', i);
+            await save('Avon, CO', i);
+            await save('Edwards, CO', i);
+            await save('Eagle, CO', i);
+            await save('Gypsum, CO', i);
+            await save('Glenwood Springs, CO', i);
+            await save('New Castle, CO', i);
+            await save('Silt, CO', i);
+            await save('Rifle, CO', i);
+            await save('Grand Junction, CO', i);
+            await save('Fruita, CO', i);
             await save('Moab, UT', i);
+            await save('Boulder, CO', i);
+            await save('Buena Vista, CO', i);
+            await save('Woodland Park, CO', i);
+            await save('Ouray, CO', i);
         }
     })();
 });
