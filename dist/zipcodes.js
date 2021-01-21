0d5b03ad4025d11dcb830a4cdb344ea02c238141
Chris Simpson <chris@nilk.net>
2021-01-21 14:05:56 -0700

    Update zipcode built js

--- dist/zipcodes.js
+++ dist/zipcodes.js
@@ -69,10 +69,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     function convertZipcode(source) {
         // write('C');
         return {
-            zip: source.Postcode.replace(/\s/g, ''),
-            primary_city: source.District.replace(/'/g, `\\'`),
-            state: source['Police force'].replace(/'/g, `\\'`),
-            county: source['Middle layer super output area'].replace(/'/g, `\\'`),
+            zip: source.Postcode.replace(/\s/g, '').trim(),
+            primary_city: source.District.replace(/'/g, `\\'`).trim(),
+            state: source.County.replace(/'/g, `\\'`).trim(),
+            county: source.Ward.replace(/'/g, `\\'`).trim(),
             timezone: null,
             area_codes: null,
             latitude: source.Latitude.toString(),
